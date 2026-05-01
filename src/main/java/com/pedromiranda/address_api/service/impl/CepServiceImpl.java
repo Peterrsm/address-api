830a5f85af19d0dc6846ec9bdc3939f7c47a4af5
peter <pedrorsmiranda@gmail.com>
2026-05-01 11:25:12 -0300

    feat: Circuit breaker
    Circuit breaker implementation

--- src/main/java/com/pedromiranda/address_api/service/impl/CepServiceImpl.java
+++ src/main/java/com/pedromiranda/address_api/service/impl/CepServiceImpl.java
@@ -5,6 +5,7 @@ import com.pedromiranda.address_api.dto.AddressResponse;
 import com.pedromiranda.address_api.model.Log;
 import com.pedromiranda.address_api.repository.LogRepository;
 import com.pedromiranda.address_api.service.interfaces.ICepService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,20 +27,42 @@ public class CepServiceImpl implements ICepService {
     }
 
     @Override
+    @CircuitBreaker(name = "addressApi", fallbackMethod = "fallbackBuscarEndereco")
     public AddressResponse buscarEndereco(String cep) {
 
         AddressResponse response = cepClient.buscarEndereco(cep);
 
         log.info("CEP {} localizado com sucesso: {}", cep, response.toString());
 
-        Log log = new Log(
+        salvarLogNoBanco(response, "SUCESSO");
+
+        return response;
+    }
+
+    public AddressResponse fallbackBuscarEndereco(String cep, Throwable e) {
+        log.error("Fallback acionado para o CEP {}. Motivo: {}", cep, e.getMessage());
+
+        AddressResponse responseFallback = new AddressResponse(
+                cep,
+                "Endereço temporariamente indisponível",
+                "Indisponível",
+                "null",
+                "null",
+                "null"
+        );
+
+        salvarLogNoBanco(responseFallback, "FALHA/CIRCUITO_ABERTO");
+
+        return responseFallback;
+    }
+
+    private void salvarLogNoBanco(AddressResponse response, String status) {
+        Log logEntity = new Log(
                 response.cep(),
                 response.logradouro(),
                 response.localidade(),
-                LocalDateTime.now());
-
-        logRepository.save(log);
-
-        return response;
+                LocalDateTime.now()
+        );
+        logRepository.save(logEntity);
     }
 }
